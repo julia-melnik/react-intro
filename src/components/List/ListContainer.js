2d8b4be3fb245bac0d30dd8d0042fa5084515d12
Julia Melnik <yuliyamielnik@gmail.com>
2020-05-13 18:51:20 +0200

    add map Dispatch To Props

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -4,11 +4,11 @@ import { getColumnsForList, createActionAddColumn } from '../../redux/columnsRed
 
 //export const getColumnsForList = ({columns}, listId) => columns.filter(column => column.listId == listId);
 
-const mapStateToProps = (state, props) => ({
+const mapStateToProps = (state, props) => ({ // dodaje propsy komponentu List, wykorzystując fragmenty stanu aplikacji z reduksowego magazynu (store).
   columns: getColumnsForList(state, props.id),     
 });
  
-const mapDispatchToProps = (dispatch, props) => ({
+const mapDispatchToProps = (dispatch, props) => ({ //dodaje propsy komponentu – jednak ich wartościami nie są dane ze stanu, ale funkcje wysyłające akcje do magazynu.
   addColumn: title => dispatch(createActionAddColumn({
     listId: props.id,
     title,
